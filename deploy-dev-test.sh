4296a3818d7fbe405abc11c6d8a8c74de83ad39b
ekeneattoh <ekeneattoh@gmail.com>
2021-07-04 18:18:24 +0200

    run actions only on push and pr for main branch

--- deploy-dev-test.sh
+++ deploy-dev-test.sh
@@ -6,5 +6,7 @@ set -e
 # navigate to the dev-test config directory
 cd dev-test/
 
+terraform init -input=false
+
 #apply the configuration with the tfvars file
 terraform apply -input=false -auto-approve -var-file=dev-test.tfvars
