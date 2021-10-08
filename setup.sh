09d41528355870d4b2e6d39f3c0acfc43a0d3dc2
ekeneattoh <ekeneattoh@gmail.com>
2021-10-08 17:08:00 +0200

    removed secret

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -e 
 
 export ARM_CLIENT_ID="ffefb7b1-1139-4c7a-8828-e56ec1802748"
-export ARM_CLIENT_SECRET="bQpkgjb.1~3SY5_6VULM2V73wYQkT.6UKZ"
+export ARM_CLIENT_SECRET="Client Secret"
 export ARM_SUBSCRIPTION_ID="201e612c-a95e-4c2e-aefe-5aef9c0cafb3"
 export ARM_TENANT_ID="45e03715-eee3-4269-b31a-dc913885ef4a"
 
